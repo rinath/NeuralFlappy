bd5b20d63e70a6ca917b6d43637584a86e7dd520
Rinat <rinath3@gmail.com>
2018-07-20 14:05:58 +0600

    Second commit

---
+++ Columns.js
@@ -0,0 +1,48 @@
+function Columns(width, height){
+  this.hole = 100;
+  this.width = width;
+  this.height = height;
+  this.w = 30;
+  //this.h = [];//Math.random() * (height * 0.9 - this.hole) + height * 0.05;
+  //this.x = [];
+  this.cols = [];
+
+  this.isColliding1 = function(birb, x, y, w, h){
+    circleDistance_x = Math.abs(birb.x - x - w/2);
+    circleDistance_y = Math.abs(birb.y - y - h/2);
+    if (circleDistance_x > (w/2 + birb.r)) { return false; }
+    if (circleDistance_y > (h/2 + birb.r)) { return false; }
+    if (circleDistance_x <= (w/2)) { return true; }
+    if (circleDistance_y <= (h/2)) { return true; }
+    cornerDistance_sq = (circleDistance_x - w/2) * (circleDistance_x - w/2) +
+                         (circleDistance_y - h/2) * (circleDistance_y - h/2);
+    return (cornerDistance_sq <= (birb.r * birb.r));
+  }
+
+  this.isColliding = function(birb){
+    for (var i = 0; i < this.cols.length; i++){
+      if (this.isColliding1(birb, this.cols[i].x, 0, this.w, this.cols[i].h) ||
+          this.isColliding1(birb, this.cols[i].x, this.cols[i].h + this.hole, this.w, this.height - this.cols[i].h - this.hole))
+          return true;
+    }
+    return false;
+  }
+
+  this.update = function(){
+    if (this.cols.length > 0 && this.cols[0].x < this.cols[0].w - this.hole * 2)
+      this.cols.splice(0, 1);
+    if (this.cols.length == 0 || this.width - this.cols[this.cols.length - 1].x > 200)
+      this.cols.push({x: this.width, h: Math.random() * (this.height * 0.9 - this.hole) + this.height * 0.05});
+    for (var i = 0; i < this.cols.length; i++){
+      this.cols[i].x -= 3;
+    }
+  }
+
+  this.draw = function(g){
+    for (var i = 0; i < this.cols.length; i++){
+      g.rect(this.cols[i].x, 0, this.w, this.cols[i].h);
+      g.rect(this.cols[i].x, this.cols[i].h + this.hole, this.w, this.height - this.cols[i].h - this.hole);
+    }
+    g.stroke();
+  }
+}
--- logic.js
+++ logic.js
@@ -3,41 +3,28 @@ var width = 600, height = 300;
 canvas.width = width;
 canvas.height = height;
 var g = canvas.getContext('2d');
-g.moveTo(10, 10);
-g.lineTo(500, 150);
-g.stroke();
 
-var birb;
-var cols = [];
+var birb, cols;
+var collision;
 startGame();
 setInterval(updateGame, 20);
 
 function startGame(){
   birb = new Birb(height);
-  cols = [];
+  cols = new Columns(width, height);
 }
 
 function updateGame(){
   birb.update();
-
-  if (cols.length > 0 && cols[0].x < cols[0].w - cols[0].hole * 2)
-    cols.splice(0, 1);
-  if (cols.length == 0 || width - cols[cols.length - 1].x > 200)
-    cols.push(new Column(width, height));
-  for (var i = 0; i < cols.length; i++){
-    cols[i].update();
-    if (cols[i].isColliding(birb))
-      birb.onCollision();
-  }
-
+  cols.update();
+  collision = cols.isColliding(birb);
   drawGame();
 }
 
 function drawGame(){
   g.clearRect(0, 0, width, height);
   birb.draw(g);
-  for (var i = 0; i < cols.length; i++)
-    cols[i].draw(g);
+  cols.draw(g);
 }
 
 window.onkeypress = function(){
